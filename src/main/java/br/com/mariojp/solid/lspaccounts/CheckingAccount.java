54bab985f6f83b078b28d3c2b1b79625e27a3c4f
mafenandaup <mafenandaup@gmail.com>
2025-08-24 15:27:13 -0300

    refactor: implementando construtores

--- src/main/java/br/com/mariojp/solid/lspaccounts/CheckingAccount.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/CheckingAccount.java
@@ -2,9 +2,13 @@ package br.com.mariojp.solid.lspaccounts;
 
 public class CheckingAccount extends Account implements Withdrawable {
 
+	public CheckingAccount(double balance) {
+		super();
+	}
+	
     @Override
     public void withdraw(double amount) {
-    	if (amount <=balance) {
+    	if (amount <=balance) { 
     		throw new IllegalArgumentException("Saldo insuficiente"); //exceção caso o saldo seja insuficiente
     	}
         super.withdraw(amount); //utiliza o método withdraw da classe pai (ACCOUNT)
